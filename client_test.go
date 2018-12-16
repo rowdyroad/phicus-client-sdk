adc386b42689717ad2bc7d0bbf6ae2b20395668e
b.strelchik <b.strelchik@gmail.com>
2018-12-17 02:40:17 +0300

    + add tests

--- client_test.go
+++ client_test.go
@@ -8,7 +8,7 @@ import (
 
 func TestMain(t *testing.T) {
 	log.SetFlags(log.Lshortfile)
-	c := NewHTTPClient("http://localhost:10080/api/measurings")
+	c := NewHTTPClient("http://localhost/api/measurings")
 	measuringID, err := c.Send(NewMeasuring("test", "test", nil, nil, nil, nil))
 	if err != nil {
 		t.Fatal(err)
@@ -24,5 +24,41 @@ func TestMain(t *testing.T) {
 	if err := c.Attach(measuringID, fileID); err != nil {
 		t.Fatal(err)
 	}
+
+}
+
+func TestAutoAttach(t *testing.T) {
+	log.SetFlags(log.Lshortfile)
+	c := NewHTTPClient("http://localhost/api/measurings")
+	fileID, err := c.Upload(bytes.NewBufferString("hello"))
+	if err != nil {
+		t.Fatal(err)
+	}
+	log.Println("FileID:", fileID)
+
+	measuringID, err := c.Send(NewMeasuring("test", "test", nil, nil, nil, []string{fileID}))
+	if err != nil {
+		t.Fatal(err)
+	}
+	log.Println("MeasuringID:", measuringID)
+
+}
+
+func TestDisplay(t *testing.T) {
+	log.SetFlags(log.Lshortfile)
+	c := NewHTTPClient("http://localhost/api/measurings")
+	fileID, err := c.UploadFile("./photo.jpg")
+	if err != nil {
+		t.Fatal(err)
+	}
+	log.Println("FileID:", fileID)
+
+	display := "<img src=\"{{UPLOAD}}/" + fileID + "\"/>"
+
+	measuringID, err := c.Send(NewMeasuring("test", "test", nil, nil, &display, []string{fileID}))
+	if err != nil {
+		t.Fatal(err)
+	}
+	log.Println("MeasuringID:", measuringID)
 
 }
